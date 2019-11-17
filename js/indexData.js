97b7ad8d7e2b3cb75d912ee7cd57db1b5636b016
huajian123 <287643967@qq.com>
2019-11-17 14:49:06 +0800

    修改为鼠标点击

--- js/indexData.js
+++ js/indexData.js
@@ -4,42 +4,42 @@ var indexData = [
         areaRatio: {
             count: 30,
             list: [
-                {name: "雨量", value: 20},
-                {name: "温度", value: 50},
-                {name: "湿度", value: 30},
-                {name: "测斜", value: 10},
-                {name: "震动", value: 30},
-                {name: "水位", value: 10}
+                {name: '金昌市', value: 20},
+                {name: '兰州市', value: 50},
+                {name: '白银市', value: 30},
+                {name: '庆阳市', value: 10},
+                {name: '天水市', value: 30},
+                {name: "嘉峪关", value: 10}
             ]
         },
         // TOP5产品排行
         proRank: [
-            {name: '兰州', price: "384,529,403"},
-            {name: '西安', price: "293,711,083"},
-            {name: '张掖', price: "254,529,403"},
-            {name: '太原', price: "213,711,083"},
-            {name: '大同', price: "114,529,403"}
+            {name: '金昌市', price: "384,529,403"},
+            {name: '兰州市', price: "293,711,083"},
+            {name: '白银市', price: "254,529,403"},
+            {name: '庆阳市', price: "213,711,083"},
+            {name: '天水市', price: "114,529,403"}
         ],
         // 区域销售金额
         areaSales: [130, 240, 450, 50, 40, 100, 90], // 11,0
         // 年度订单金额
         yearAreaSale: [
-            {name: "雨量", value: 0},
-            {name: "温度", value: 1000000},
-            {name: "湿度", value: 0},
-            {name: "测斜", value: 0},
-            {name: "震动", value: 0},
-            {name: "水位", value: 0}
+            {name: '金昌市', value: 0},
+            {name: '兰州市', value: 100},
+            {name: '白银市', value: 0},
+            {name: '庆阳市', value: 0},
+            {name: '天水市', value: 0},
+            {name: "嘉峪关", value: 0}
         ],
         // 销售目标达成率
         saleReachRate: [100, 190],
         // TOP5客户排行
         cusRank: [
-            {name: '兰州', price: "88%"},
-            {name: '西宁', price: "94%"},
-            {name: '西安', price: "57%"},
-            {name: '银川', price: "62%"},
-            {name: '南宁', price: "77%"}
+            {name: '金昌市', price: "88%"},
+            {name: '兰州市', price: "94%"},
+            {name: '白银市', price: "57%"},
+            {name: '庆阳市', price: "62%"},
+            {name: '天水市', price: "77%"}
         ],
         // 区域达成率
         areaReachRate: [10, 20, 32, 2, 28, 12, 90]
@@ -49,42 +49,42 @@ var indexData = [
         areaRatio: {
             count: 40,
             list: [
-                {name: "雨量", value: 20},
-                {name: "温度", value: 50},
-                {name: "湿度", value: 30},
-                {name: "测斜", value: 10},
-                {name: "水位", value: 30},
-                {name: "震动", value: 10}
+                {name: '金昌市', value: 20},
+                {name: '兰州市', value: 50},
+                {name: '白银市', value: 30},
+                {name: '庆阳市', value: 10},
+                {name: '天水市', value: 30},
+                {name: "嘉峪关", value: 10}
             ]
         },
         // TOP5产品排行
         proRank: [
-            {name: '南京', price: "78,4529,403"},
-            {name: '北京', price: "49,3711,083"},
-            {name: '贵州', price: "35,4529,403"},
-            {name: '桂林', price: "21,3711,083"},
-            {name: '呼和浩特', price: "11,4529,403"}
+            {name: '金昌市', price: "78,4529,403"},
+            {name: '兰州市', price: "49,3711,083"},
+            {name: '白银市', price: "35,4529,403"},
+            {name: '庆阳市', price: "21,3711,083"},
+            {name: '天水市', price: "11,4529,403"}
         ],
         // 区域销售金额
         areaSales: [130, 240, 450, 100, 90, 100, 90], //12,1
         // 年度订单金额
         yearAreaSale: [
-            {name: "东北", value: 1100000},
-            {name: "华北", value: 0},
-            {name: "西北", value: 0},
-            {name: "华东", value: 0},
-            {name: "华中", value: 0},
-            {name: "华南", value: 0},
-            {name: "西南", value: 0}
+            {name: "金昌市", value: 1100000},
+            {name: "兰州市", value: 0},
+            {name: "白银市", value: 0},
+            {name: "庆阳市", value: 0},
+            {name: "天水市", value: 0},
+            {name: "酒泉市", value: 0},
+            {name: "陇南市", value: 0}
         ],
         // 销售目标达成率
         saleReachRate: [100, 210],
         cusRank: [
-            {name: '兰州', price: "45%"},
-            {name: '张掖', price: "83%"},
-            {name: '甘南', price: "29%"},
-            {name: '成都', price: "83%"},
-            {name: '重庆', price: "72%"}
+            {name: '金昌市', price: "45%"},
+            {name: '兰州市', price: "83%"},
+            {name: '白银市', price: "29%"},
+            {name: '庆阳市', price: "83%"},
+            {name: '天水市', price: "72%"}
         ],
         // 区域达成率
         areaReachRate: [10, 30, 32, 2, 88, 12, 90]
@@ -94,42 +94,42 @@ var indexData = [
         areaRatio: {
             count: 60,
             list: [
-                {name: "华东", value: 20},
-                {name: "华中", value: 50},
-                {name: "华北", value: 30},
-                {name: "华南", value: 10},
-                {name: "东北", value: 30},
-                {name: "西南", value: 10},
-                {name: "西北", value: 50},
+                {name: "金昌市", value: 20},
+                {name: "兰州市", value: 50},
+                {name: "白银市", value: 30},
+                {name: "庆阳市", value: 10},
+                {name: "天水市", value: 30},
+                {name: "酒泉市", value: 10},
+                {name: "陇南市", value: 50},
             ]
         },
         // TOP5产品排行
         proRank: [
-            {name: '西安', price: "784,529,403"},
-            {name: '济南', price: "493,711,083"},
-            {name: '石家庄', price: "354,529,403"},
-            {name: '张家口', price: "213,711,083"},
-            {name: '郑州', price: "114,529,403"}
+            {name: '金昌市', price: "784,529,403"},
+            {name: '兰州市', price: "493,711,083"},
+            {name: '白银市', price: "354,529,403"},
+            {name: '庆阳市', price: "213,711,083"},
+            {name: '天水市', price: "114,529,403"}
         ],
         // 区域销售金额
         areaSales: [130, 240, 450, 200, 90, 100, 90], //22,1
         // 年度订单金额
         yearAreaSale: [
-            {name: "东北", value: 0},
-            {name: "华北", value: 10000000},
-            {name: "西北", value: 0},
-            {name: "华东", value: 10},
-            {name: "华中", value: 0},
-            {name: "华南", value: 0},
-            {name: "西南", value: 0}
+            {name: "金昌市", value: 0},
+            {name: "兰州市", value: 10000000},
+            {name: "白银市", value: 0},
+            {name: "庆阳市", value: 10},
+            {name: "天水市", value: 0},
+            {name: "酒泉市", value: 0},
+            {name: "陇南市", value: 0}
         ],
         saleReachRate: [100, 750],
         cusRank: [
-            {name: '兰州', price: "80%"},
-            {name: '郑州', price: "73%"},
-            {name: '南京', price: "56%"},
-            {name: '贵阳', price: "93%"},
-            {name: '昆明', price: "67%"}
+            {name: '金昌市', price: "80%"},
+            {name: '兰州市', price: "73%"},
+            {name: '白银市', price: "56%"},
+            {name: '庆阳市', price: "93%"},
+            {name: '天水市', price: "67%"}
         ],
         // 区域达成率
         areaReachRate: [10, 70, 36, 22, 28, 12, 90]
@@ -139,42 +139,42 @@ var indexData = [
         areaRatio: {
             count: 80,
             list: [
-                {name: "华东", value: 20},
-                {name: "华中", value: 50},
-                {name: "华北", value: 30},
-                {name: "华南", value: 10},
-                {name: "东北", value: 30},
-                {name: "西南", value: 10},
-                {name: "西北", value: 50},
+                {name:"金昌市", value: 20},
+                {name:"兰州市", value: 50},
+                {name:"白银市", value: 30},
+                {name:"庆阳市", value: 10},
+                {name:"天水市", value: 30},
+                {name:"酒泉市", value: 10},
+                {name:"陇南市", value: 50},
             ]
         },
         // TOP5产品排行
         proRank: [
-            {name: '西安', price: "74,529,403"},
-            {name: '济南', price: "43,711,083"},
-            {name: '石家庄', price: "34,529,403"},
-            {name: '张家口', price: "21,711,083"},
-            {name: '郑州', price: "11,529,403"}
+            {name: '金昌市', price: "74,529,403"},
+            {name: '兰州市', price: "43,711,083"},
+            {name: '白银市', price: "34,529,403"},
+            {name: '庆阳市', price: "21,711,083"},
+            {name: '天水市', price: "11,529,403"}
         ],
         // 区域销售金额
         areaSales: [330, 440, 450, 400, 490, 100, 90], //34,1
         // 年度订单金额
         yearAreaSale: [
-            {name: "东北", value: 0},
-            {name: "华北", value: 0},
-            {name: "西北", value: 12000000},
-            {name: "华东", value: 0},
-            {name: "华中", value: 0},
-            {name: "华南", value: 10},
-            {name: "西南", value: 0}
+            {name: "金昌市", value: 0},
+            {name: "兰州市", value: 0},
+            {name: "白银市", value: 12000000},
+            {name: "庆阳市", value: 0},
+            {name: "天水市", value: 0},
+            {name: "酒泉市", value: 10},
+            {name: "陇南市", value: 0}
         ],
         saleReachRate: [100, 620],
         cusRank: [
-            {name: '张家口', price: "88%"},
-            {name: '郑州', price: "45%"},
-            {name: '洛阳', price: "95%"},
-            {name: '开封', price: "33%"},
-            {name: '兰考', price: "84%"}
+            {name: '金昌市', price: "88%"},
+            {name: '兰州市', price: "45%"},
+            {name: '白银市', price: "95%"},
+            {name: '庆阳市', price: "33%"},
+            {name: '天水市', price: "84%"}
         ],
         //
         areaReachRate: [10, 15, 32, 42, 6, 29, 30]
